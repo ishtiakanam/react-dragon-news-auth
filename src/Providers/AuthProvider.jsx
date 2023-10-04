d64e71699ce6b70839b157d5dab5833f748f701d
ishtiakanam <iafahim@gmail.com>
2023-10-04 12:03:58 +0600

    firebase updated

---
+++ src/Providers/AuthProvider.jsx
@@ -0,0 +1,48 @@
+/* eslint-disable react/prop-types */
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import auth from "../firebase/firebase.config";
+
+export const AuthContext = createContext(null)
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    const createUser = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const logOut = () => {
+        setLoading(true)
+        return signOut(auth)
+            .then(result => { console.log(result) })
+            .catch(err => { console.log(err) })
+    }
+
+    const signIn = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
+            console.log('Hey you go from here now', currentUser);
+            setUser(currentUser)
+            setLoading(false)
+        })
+        return () => {
+            unSubscribe()
+        }
+    }, [])
+    const authInfo = { user, createUser, logOut, signIn, loading }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,77 @@
+import { Link } from "react-router-dom";
+import Navbar from "../Shared/Navbar/Navbar";
+import { useContext } from "react";
+import { AuthContext } from "../../Providers/AuthProvider";
+
+const Register = () => {
+
+    const { createUser } = useContext(AuthContext)
+
+    const handleRegister = e => {
+        e.preventDefault()
+        // const email = e.target.email.value
+        // const password = e.target.password.value
+        console.log(e.currentTarget);
+        const form = new FormData(e.currentTarget)
+        const name = form.get('name')
+        const photo = form.get('photo')
+        const email = form.get('email')
+        const password = form.get('password')
+        console.log(name, photo, email, password);
+        // console.log(form.get('password'))
+
+        // crate user
+        createUser(email, password)
+            .then(result => {
+                console.log(result);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+    return (
+        <div>
+            <Navbar></Navbar>
+            <div>
+                <h2 className="text-2xl my-10 text-center">Register please</h2>
+
+                <form onSubmit={handleRegister} className="md:w-3/4 lg:w-1/2 mx-auto">
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input type="text" name="name" placeholder="Name" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Photo url</span>
+                        </label>
+                        <input type="text" name="photo" placeholder="Photo-URL" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" name="email" placeholder="Email" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password" name="password" placeholder="Password" className="input input-bordered" required />
+                        <label className="label">
+                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                        </label>
+                    </div>
+                    <div className="form-control mt-6">
+                        <button className="btn btn-primary">Register</button>
+                    </div>
+                </form>
+                <p className="text-center mt-2">Already have an account?<Link className="text-blue-700 font-bold" to='/login'>Login</Link></p>
+            </div>
+
+        </div>
+    );
+};
+
+export default Register;
